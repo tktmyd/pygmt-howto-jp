ec31c9da6e42ddfffdeefc426004366ca40ac93e
Takuto Maeda <6821825+tktmyd@users.noreply.github.com>
2026-03-14 14:17:38 +0900

    trying to fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rif src/_build/html
 cd src
 export BASE_URL="/pygmt-howto-jp" 
-myst build --html --execute
+myst build --html --execute --execute-parallel 1
 cd ..
 touch src/_build/html/.nojekyll
 rsync -a --delete src/_build/html/ ./docs/ 
